1ecf890d679aeecd08b0cf1749bb5efb4d421ce5
BCox330 <cox.815@aol.com>
2019-07-15 09:58:07 -0400

    first commit

---
+++ src/main/java/com/example/Mastery/Project/MasteryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mastery.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteryProjectApplication.class, args);
+	}
+
+}
